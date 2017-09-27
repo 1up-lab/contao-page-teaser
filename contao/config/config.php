7401b14231b9461863805e3b2527a4f569abaaea
David Greminger <david.greminger@1up.io>
2017-09-27 11:53:39 +0200

    Added blank option to image sizes

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['FE_MOD']['navigationMenu']['page_teasers'] = 'Oneup\PageTeaser\Module\PageTeaser';
-$GLOBALS['TL_CTE']['links']['page_teasers']          = 'Oneup\PageTeaser\Element\PageTeaser';
+$GLOBALS['TL_CTE']['links']['page_teasers'] = 'Oneup\PageTeaser\Element\PageTeaser';
--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -6,36 +6,36 @@ $GLOBALS['TL_DCA']['tl_content']['palettes'] += [
 
 $GLOBALS['TL_DCA']['tl_content']['fields'] += [
     'teasers' => [
-        'label'                   => &$GLOBALS['TL_LANG']['tl_content']['teasers'],
-        'exclude'                 => true,
-        'inputType'               => 'pageTree',
-        'foreignKey'              => 'tl_page.title',
-        'eval'                    => [
-            'multiple'   => true,
-            'fieldType'  => 'checkbox',
-            'files'      => true,
+        'label' => &$GLOBALS['TL_LANG']['tl_content']['teasers'],
+        'exclude' => true,
+        'inputType' => 'pageTree',
+        'foreignKey' => 'tl_page.title',
+        'eval' => [
+            'multiple' => true,
+            'fieldType' => 'checkbox',
+            'files' => true,
             'orderField' => 'teasers_order',
-            'mandatory'  => true
+            'mandatory' => true
         ],
-        'sql'                     => "blob NULL",
-        'relation'                => [
+        'sql' => "blob NULL",
+        'relation' => [
             'type' => 'hasMany',
             'load' => 'lazy'
         ],
     ],
     'teasers_order' => [
-        'label'                   => &$GLOBALS['TL_LANG']['tl_content']['teasers_order'],
-        'sql'                     => "blob NULL",
+        'label' => &$GLOBALS['TL_LANG']['tl_content']['teasers_order'],
+        'sql' => "blob NULL",
     ],
     'teasers_template' => [
-        'label'                   => &$GLOBALS['TL_LANG']['tl_content']['teasers_template'],
-        'default'                 => 'teasers_list',
-        'exclude'                 => true,
-        'inputType'               => 'select',
-        'options_callback'        => [
+        'label' => &$GLOBALS['TL_LANG']['tl_content']['teasers_template'],
+        'default' => 'teasers_list',
+        'exclude' => true,
+        'inputType' => 'select',
+        'options_callback' => [
             'Oneup\PageTeaser\Helper\DcaHelper',
             'getTeaserTemplate'
         ],
-        'sql'                     => "varchar(32) NOT NULL default ''",
+        'sql' => "varchar(32) NOT NULL default ''",
     ],
 ];
