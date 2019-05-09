b54e72f3e0e70d8b7fe19c7ecce3375addafd7a9
Jim Schmid <jim.schmid@1up.io>
2019-05-09 11:22:21 +0200

    Refactored legacy contao directory

--- contao/config/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-TemplateLoader::addFiles([
-    'mod_pageteasers' => 'system/modules/page-teaser/templates',
-    'ce_pageteasers' => 'system/modules/page-teaser/templates',
-    'picture_teaser' => 'system/modules/page-teaser/templates',
-    'teasers_list' => 'system/modules/page-teaser/templates',
-]);
--- contao/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['FE_MOD']['navigationMenu']['page_teasers'] = 'Oneup\PageTeaser\Module\PageTeaser';
-$GLOBALS['TL_CTE']['links']['page_teasers'] = 'Oneup\PageTeaser\Element\PageTeaser';
