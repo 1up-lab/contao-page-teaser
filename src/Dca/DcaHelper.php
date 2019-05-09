1ee1794540dc874daebea84a036f8f63d874e779
Jim Schmid <jim.schmid@1up.io>
2019-05-09 11:05:52 +0200

    Added Bundle class

---
+++ src/Dca/DcaHelper.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneup\PageTeaser\Dca;
+
+use Contao\Config;
+use Contao\Controller;
+use Contao\DataContainer;
+use Contao\FilesModel;
+use Contao\Input;
+use Contao\PageModel;
+use Contao\StringUtil;
+use Doctrine\DBAL\Connection;
+
+class DcaHelper
+{
+    protected $database;
+
+    public function __construct(Connection $database)
+    {
+        $this->database = $database;
+    }
+
+    /**
+     * Return all news templates as array
+     * @return array
+     */
+    public function getTeaserTemplate(): array
+    {
+        return Controller::getTemplateGroup('teasers_');
+    }
+
+    /**
+     * Dynamically add flags to the "singleSRC" field
+     *
+     * @param $varValue
+     * @param DataContainer $dc
+     *
+     * @return mixed
+     */
+    public function setSingleSrcFlags($varValue, DataContainer $dc)
+    {
+        if ($dc->activeRecord) {
+            switch ($dc->activeRecord->type) {
+                case 'text':
+                case 'hyperlink':
+                case 'image':
+                case 'accordionSingle':
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] = Config::get('validImageTypes');
+                    break;
+
+                case 'download':
+                    $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['extensions'] = Config::get('allowedDownload');
+                    break;
+            }
+        }
+
+        return $varValue;
+    }
+
+    /**
+     * Pre-fill the "alt" and "caption" fields with the file meta data
+     *
+     * @param $varValue
+     * @param DataContainer $dc
+     *
+     * @return mixed
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function storeFileMetaInformation($varValue, DataContainer $dc)
+    {
+        if ($dc->activeRecord->singleSRC == $varValue) {
+            return $varValue;
+        }
+
+        $objFile = FilesModel::findByUuid($varValue);
+
+        if ($objFile !== null) {
+            $arrMeta = StringUtil::deserialize($objFile->meta);
+
+            if (!empty($arrMeta)) {
+
+                /** @var PageModel $objPage */
+                $objPage = $this->database->prepare("SELECT * FROM tl_page WHERE id=(SELECT pid FROM " . ($dc->activeRecord->ptable ?: 'tl_article') . " WHERE id=?)")
+                    ->execute($dc->activeRecord->pid);
+
+                if ($objPage->numRows) {
+                    $objModel = new PageModel();
+                    $objModel->setRow($objPage->row());
+                    $objModel->loadDetails();
+
+                    // Convert the language to a locale (see #5678)
+                    $strLanguage = str_replace('-', '_', $objModel->rootLanguage);
+
+                    if (isset($arrMeta[$strLanguage])) {
+                        Input::setPost('alt', $arrMeta[$strLanguage]['title']);
+                        Input::setPost('caption', $arrMeta[$strLanguage]['caption']);
+                    }
+                }
+            }
+        }
+
+        return $varValue;
+    }
+}
---
+++ src/OneupContaoPageTeaserBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneup\PageTeaser;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OneupContaoPageTeaserBundle extends Bundle
+{
+
+}
